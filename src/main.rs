eee423edb6679a3ac995bd43edf10ec7aa2f3f1c
john <john@shue.com>
2026-03-10 17:51:57 +0300

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 mod cli;
 use std::env;
+use dotenvy::dotenv;
+mod vps_config;
+mod config;
+use config::Config;
 
 fn main() {
+    dotenv().ok();
+    let config = Config::init();
+
     let args: Vec<String> = env::args().collect();
     match args.get(1).map(|val| String::as_str(val)) {
-        Some("init") => cli::commands::init(),
+        Some("init") => cli::commands::init(config),
         Some(cmd) => eprintln!("Unknown command: {cmd}"),
         None => eprintln!("Usage: vpsctl <command>"),
     };
